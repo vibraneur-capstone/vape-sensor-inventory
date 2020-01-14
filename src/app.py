4246f578f8d7920c07b239e524bae5d20d762ae6
BenxinNiu <benxin.niu@mun.ca>
2020-01-14 20:16:56 -0330

    update test endpoint

--- src/app.py
+++ src/app.py
@@ -1,15 +1,24 @@
 import connexion
 import repositories.db as DB
 import repositories.models as models
+
 app = connexion.App(__name__, specification_dir='./')
 app.add_api('swagger.yaml')
+DB.connect_db()
 
-#test to insert data to the data base, meant to be removed later
+
+# test to insert data to the data base, meant to be removed later
+# TODO: remove this... and implement unit test
 @app.route("/test")
 def test():
-    org = models.Organization(organization_name = "Husky",
-    organization_city = "St. Johns")
-    org.save()
-    return "created a doc using mongoengine"
+    org = models.Organization(organization_name="Husky",
+                              organization_address="St. Johns")
+    sensor = models.Sensor(sensor_name="test name",
+                           sensor_status=models.SensorStatus.DECOMMISSIONED,
+                           organization=org)
+    sensor.save()
+    return sensor.to_json()
+
+
 if __name__ == '__main__':
     app.run(port=8822)
