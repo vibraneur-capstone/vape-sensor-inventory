7e11a0e536ce000a801396ff6e208414d1a5649b
Benxin Niu <bn2645@mun.ca>
2020-01-14 20:25:50 -0330

    Fix/mongo document (#7)
    * fix mongo document model
    
    * update test endpoint
    
    * rename mongo configuration file
    
    * refactor connect method
    
    * bump up patch version

--- src/repositories/models.py
+++ src/repositories/models.py
@@ -1,17 +1,31 @@
 from mongoengine import *
+from enum import Enum
 
-class Organization(Document):
+
+class Organization(EmbeddedDocument):
     organization_name = StringField(required=True, max_length=50)
-    organization_city = StringField(required=False, max_length=50)
-    total_sensor_number = IntField(required=True, default=0)
+    organization_address = StringField(required=False, max_length=50)
+    total_sensor_count = IntField(required=True, default=0)
+    total_machine_count = IntField(required=True, default=0)
 
-class Machine(Document):
-    name = StringField(required=True)
-    organization_id = ReferenceField(Organization, required=False)
+
+class Machine(EmbeddedDocument):
+    machine_name = StringField(required=True)
+    organization_name = StringField(required=True, max_length=50)
+    location = StringField(required=False)
+    total_sensor_count = IntField(required=True, default=0)
+
 
 class Sensor(Document):
+    id = StringField(required=False)
     sensor_name = StringField(required=True)
-    organization_id = ReferenceField(Organization, required=True)
-    machine_id = ReferenceField(Machine, required=True)
+    organization = EmbeddedDocumentField(Organization, required=False)
+    machine = EmbeddedDocumentField(Machine, required=False)
     sensor_install_time = DateField(required=False)
     sensor_status = StringField(required=True, max_length=50)
+
+
+class SensorStatus(str, Enum):
+    ONLINE = "ONLINE"
+    DECOMMISSIONED = "DECOMMISSIONED"
+    OFFLINE = "OFFLINE"
---
+++ src/repositories/mongo_config.py
@@ -0,0 +1,5 @@
+import ssl
+
+MONGO_CONNECTION_STRING = "mongodb+srv://db-vape-sensor-inventory:theMostSecurePassword?!@cluster-vape-sensor-inventory-fcfqt.mongodb.net/inventory?retryWrites=true&w=majority"
+DATABASE_NAME = "inventory"
+MONGO_CERT = ssl.CERT_NONE
