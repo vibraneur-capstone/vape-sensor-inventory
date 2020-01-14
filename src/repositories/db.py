05e1ea7df40f69d1cda2e8dfc00fed8e8e695f75
Linqiao Xia <summerxialinqiao@gmail.com>
2020-01-14 14:07:32 -0330

    added mongoengine to requirements.txt

--- src/repositories/db.py
+++ src/repositories/db.py
@@ -3,5 +3,4 @@ from .connection_string import MONGO_CONNECTION_STRING
 from mongoengine import *
 import ssl
 
-
 db = connect('db_sensor', host = MONGO_CONNECTION_STRING, ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
