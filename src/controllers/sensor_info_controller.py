158d56ebe676a59bfd3646fb29e091322e17ccc6
Noah Gallant <noah.gallant@mun.ca>
2020-01-23 19:32:46 -0330

    updated sample data

--- src/controllers/sensor_info_controller.py
+++ src/controllers/sensor_info_controller.py
@@ -5,11 +5,36 @@ def get_all_sensors(org_name, machine_name, status):
     # TODO implement actual logic... this is just for testing
     logging.info("HTTP request received with org_name: %s machine_name: %s status: %s".format(org_name, machine_name, status))
     # TODO return dummy response
-    return {"org_name": org_name, "machine_name": machine_name, "status": status}
+    response = {
+        "sensorList": [
+            {
+                "id": 'b14567',
+                "status": 'ONLINE'
+            },
+            {
+                "id": 'b14568',
+                "status": 'OFFLINE'
+            },
+            {
+                "id": 'b14569',
+                "status": 'DECOMMISSIONED'
+            },
+            {
+                "id": 'b14570',
+                "status": 'ONLINE'
+            },
+            {
+                "id": 'b14571',
+                "status": 'ONLINE'
+            },
+        ],
+        "count": 5
+    }
 
+    return response
 
 def get_sensor_detail(org_name, machine_name, sensor_id):
     # TODO implement actual logic... this is just for testing
     logging.info("HTTP request received with org_name: %s machine_name: %s id: %s".format(org_name, machine_name, sensor_id))
     # TODO return dummy response
-    return {"org_name": org_name, "machine_name": machine_name, "id": sensor_id}
+    return {"org_name": org_name, "machine_name": machine_name, "id": sensor_id, "status": 'ONLINE'}
