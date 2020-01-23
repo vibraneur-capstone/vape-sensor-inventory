c4b2410728a374e3986176ce8596bb6c15cf924b
Benxin Niu <bn2645@mun.ca>
2020-01-23 17:23:13 -0330

    Feature/enable cors (#10)
    * import flask cors
    
    * enable cors for Flask

--- src/app.py
+++ src/app.py
@@ -2,15 +2,21 @@ import connexion
 import repositories.db as DB
 import repositories.models as models
 from healthcheck import HealthCheck
+from flask_cors import CORS
 
 app = connexion.App(__name__, specification_dir='./')
 app.add_api('swagger.yaml')
+
+# Api health check
 app_health = HealthCheck()
+# Mongo connection
 DB.connect_db()
 
-# TODO: override app_health to include mongoDB connection test
+# Api health check
 app.add_url_rule("/vapehealthcheck", "vapehealthcheck", view_func=lambda: app_health.run())
 
+# enable CORS
+cors = CORS(app.app)
 
 # test to insert data to the data base, meant to be removed later
 # TODO: remove this... and implement unit test
